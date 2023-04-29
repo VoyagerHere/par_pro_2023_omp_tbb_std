64430325b7f792ea490d6bd154e51f466ead1445
VoyagerHere <kolessnikoff.d.e@gmail.com>
2023-04-29 16:55:08 +0300

    upd_2

--- modules/task_2/kolesnikov_d_integral_omp/integral.cpp
+++ modules/task_2/kolesnikov_d_integral_omp/integral.cpp
@@ -12,25 +12,25 @@
 
 double integrate_seq(
   double (*f)(double x, double y, double z),
-  double a[3],
-  double b[3],
+  double l[3],
+  double r[3],
   int n[3]
 ) {
     double result = 0.0;
 
     double h[3];
     for (int i = 0; i < 3; i++) {
-        h[i] = (b[i] - a[i]) / n[i];
+        h[i] = (r[i] - l[i]) / n[i];
     }
 
     for (int i = 0; i < n[0]; i++) {
         for (int j = 0; j < n[1]; j++) {
             for (int k = 0; k < n[2]; k++) {
-                double x = a[0] + h[0] * i;
-                double y = a[1] + h[1] * j;
-                double z = a[2] + h[2] * k;
-                double value = f(x + h[0] / 2, y + h[1] / 2, z + h[2] / 2);
-                result += h[0] * h[1] * h[2] * value;
+                double x = l[0] + hx * i;
+                double y = l[1] + hy * j;
+                double z = l[2] + hz * k;
+                double value = f(x + hx / 2, y + hy / 2, z + hz / 2);
+                result += hx * hy * hz * value;
             }
         }
     }
@@ -42,26 +42,25 @@ double integrate_seq(
 
 double integrate_prl(
   double (*f)(double x, double y, double z),
-  double a[3],
-  double b[3],
+  double l[3],
+  double r[3],
   int n[3]
 ) {
     double result = 0.0;
 
-    double h[3];
-    for (int i = 0; i < 3; i++) {
-        h[i] = (b[i] - a[i]) / n[i];
-    }
+    
+    double hx = (r[i] - l[i]) / n[i];
+    double hy = (r[i] - l[i]) / n[i];
+    double hz = (r[i] - l[i]) / n[i];
 
     #pragma omp parallel for reduction(+: result)
         for (int i = 0; i < n[0]; i++) {
             for (int j = 0; j < n[1]; j++) {
                 for (int k = 0; k < n[2]; k++) {
-                    double x = a[0] + h[0] * i;
-                    double y = a[1] + h[1] * j;
-                    double z = a[2] + h[2] * k;
-                    double value = f(x + h[0] / 2, y + h[1] / 2, z + h[2] / 2);
-                    result += h[0] * h[1] * h[2] * value;
+                    double x = l[0] + hx * i;
+                    double y = l[1] + hy * j;
+                    double z = l[2] + hz * k;
+                    result += hx * hy * hz * f(x + hx / 2, y + hy / 2, z + hz / 2);
                 }
             }
         }
